2813fa4db86c711923b674633cb224ccfdab9a1a
AdrianPiascik <ezio.141.96@gmail.com>
2019-05-09 21:58:43 +0200

    Enemies anim

--- JamHome/Assets/Scripts/EnemyBackForth.cs
+++ JamHome/Assets/Scripts/EnemyBackForth.cs
@@ -4,12 +4,15 @@ using UnityEngine;
 
 public class EnemyBackForth : MonoBehaviour {
 
+    public Animator animator;
     public float walkingSpeed=300f;
     private int currentDirection=1;
 	
 	// Update is called once per frame
 	void Update () {
-        GetComponent<Rigidbody2D>().velocity = new Vector2(currentDirection*walkingSpeed * Time.deltaTime, GetComponent<Rigidbody2D>().velocity.y);
+        var speed = currentDirection * walkingSpeed * Time.deltaTime;
+        animator.SetFloat("Speed", Mathf.Abs(speed));
+        GetComponent<Rigidbody2D>().velocity = new Vector2(speed, GetComponent<Rigidbody2D>().velocity.y);
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
